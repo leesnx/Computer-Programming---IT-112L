70cfb43df400cfe706fea696792c3f31258a234d
leesnx <112876825+leesnx@users.noreply.github.com>
2022-09-05 22:29:28 +0800

    Add files via upload

--- myProject_9415/src/Prelim/Exercises/Dispenser.java
+++ myProject_9415/src/Prelim/Exercises/Dispenser.java
@@ -1,10 +1,10 @@
 /*
  * Author: Bagsan, Lei Ceasar A.
- * Programming Date: 08/22/2022
+ * Programming Date: 09/05/2022
  * Activity Name and Number: Prelims Exercise Number 7
  */
 
-package prelim.exercises;
+package Prelim.Exercises;
 
 import java.lang.*; // import statement
 import java.util.Scanner;
@@ -16,37 +16,30 @@ public class Dispenser { // class declaration
         System.out.print("Enter amount to be withdrawn, whole numbers only: ");
         int amount = Integer.parseInt(dmc.nextLine());
 
-        int otb = 0;
-        int fhb = 0;
-        int ohb = 0;
-        double otbc = 0;
-        double fhbc = 0;
-        double ohbc = 0;
+        int otb = amount / 1000;
+            amount = amount % 1000;
+        int fhb = amount / 500;
+            amount = amount % 500;
+        int ohb = amount / 100;
+            amount = amount % 100;
 
-        while (amount > 1000){
-            otb++;
-            otbc = otbc + 1000;
-            amount = amount - 1000;
-        }
-        while (amount >= 500 && amount <= 1000){
-            fhb++;
-            fhbc = fhbc + 500;
-            amount = amount - 500;
-        }
-        while (amount >= 100 && amount < 500) {
-            ohb++;
-            ohbc = ohbc + 100;
-            amount = amount - 100;
-        }
+        double otbc = otb * 1000;
+        double fhbc = fhb * 500;
+        double ohbc = ohb * 100;
+
         int bc = otb + fhb + ohb;
-        double at = otbc + fhbc + ohbc;
+        double bt = otbc + fhbc + ohbc;
 
-        System.out.println("PESO BILLS          QUANTITY          AMOUNT");
-        System.out.println("==========         =========         =======");
-        System.out.printf("P1000                     " + otb + "          %.2f", otbc);
-        System.out.printf("\nP500                      " + fhb + "           %.2f", fhbc);
-        System.out.printf("\nP100                      " + ohb + "           %.2f", ohbc);
-        System.out.println("\n----------         ---------         -------");
-        System.out.printf("Total No. Of Bills        " + bc + "          %.2f", at);
+        System.out.println("------------------------------");
+        System.out.printf("%s%10s%10s%n","PESO BILLS","QUANTITY","AMOUNT");
+        System.out.printf("%s%13d%10.2f%n","1000.00",otb,otbc);
+        System.out.printf("%s%13d%10.2f%n","5000.00",fhb,fhbc);
+        System.out.printf("%s%14d%10.2f%n","100.00",ohb,ohbc);
+        System.out.println("------------------------------");
+        System.out.printf("%s%2d%10.2f%n","Total No. of Bills",bc,bt);
+        System.out.println("------------------------------");
+        System.out.printf("%s%23d%n","Balance",amount);
+        System.out.println("------------------------------");
+
     } // end of main method
 } // end of Dispenser class
