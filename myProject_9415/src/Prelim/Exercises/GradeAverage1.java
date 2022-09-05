e4fe567226135a5a5c0ab41fa3c8c8bd1bbaffc2
leesnx <112876825+leesnx@users.noreply.github.com>
2022-09-05 22:40:02 +0800

    Update GradeAverage1.java

--- myProject_9415/src/Prelim/Exercises/GradeAverage1.java
+++ myProject_9415/src/Prelim/Exercises/GradeAverage1.java
@@ -4,7 +4,7 @@
  * Activity Name and Number: Prelims Exercise Number 9
  */
 
-package prelim.exercises;
+package Prelim.Exercises;
 
 import java.lang.*; // import statement
 import java.util.Scanner;
